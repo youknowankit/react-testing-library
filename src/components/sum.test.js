028145a4f7d77ac5940d0851aa03d5d8200fa3a9
Ankit Yadav <thisisankityadav@gmail.com>
2026-01-08 15:21:54 +0530

    Add test case for sum

--- src/components/sum.test.js
+++ src/components/sum.test.js
@@ -1,8 +1,19 @@
 import sum from "./sum";
 
-// test("test_description", callbackFn, timeout-(optional));
-// test("test_description", () => {}, 3000);
+/*
+test("test_description", callbackFn, timeout-(optional));
+test("test_description", () => {}, 3000);
 
 test("sum_function_test", () => {
-  expect(sum(10, 10)).toBe(20);
+    expect(sum(10, 10)).toBe(20);
+});
+*/
+
+//Writing test for sum function
+test("testing sum function", () => {
+  let a = 10;
+  let b = 10;
+  let c = 30;
+  let output = 50;
+  expect(sum(a, b, c)).toBe(output);
 });
